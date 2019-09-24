527bb472d6a52e6460ed11924b6b0b77b3b6df50
Breno Beierstedt <breno@iocom.com.br>
2019-09-24 15:13:15 -0300

    Adding sh to local instance start

---
+++ install_prod.sh
@@ -0,0 +1,30 @@
+printf "##### Building project database image..."
+docker build -t iocom-neo4j ./neo4j
+printf "##### Building project base image..."
+docker build -t iocom-nodejs ./nodejs
+printf "###Build Done!"
+
+cd ..
+
+printf "\n\n#####  Cloning Platform..."
+git clone git@gitlab.com:iocom-platform/iocom-adm.git
+
+git clone git@gitlab.com:iocom-platform/iocom-api.git
+
+printf "\n\n##### Copying composer..."
+cp iocom/docker-compose.yml.sample docker-compose.yml
+
+rm proxy.conf
+
+echo "### Creating proxy.conf"
+cat > proxy.conf <<EOL
+server_tokens off;
+client_max_body_size 200m;
+EOL
+echo " "
+
+
+
+cd iocom
+
+printf "\n\nAll done! Have a nice day!\n"
